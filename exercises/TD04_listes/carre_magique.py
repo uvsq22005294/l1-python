ad57043ca9442d8a658e70e76553da4ea4c4ff70
Jeremy Morcos Doueihy <jeremy.morcos-doueihy@ens.uvsq.fr>
2020-12-05 10:41:03 +0100

    change

--- exercises/TD04_listes/carre_magique.py
+++ exercises/TD04_listes/carre_magique.py
@@ -1,33 +1,107 @@
 carre_mag = [[4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 3, 13]]
 print(carre_mag)
-print("")
+print()
+
 carre_pas_mag = [list(L) for L in carre_mag]
 carre_pas_mag[3][2] = 7
 print(carre_pas_mag)
-print("")
+print()
 
 
 def afficheCarre(carre):
     """ Affiche la liste à 2 dimensions carre comme un carré"""
-    for L in carre:
-        print(L)
-    print()
-    #return print("", carre[0], "\n", carre[1], "\n", carre[2], "\n", carre[3])
+    for i in range(len(carre)):
+        print(carre[i])
 
 
 afficheCarre(carre_mag)
-print("")
+print()
 afficheCarre(carre_pas_mag)
-print("")
+print()
 
 
 def testLignesEgales(carre):
     """ Renvoie la somme des éléments d'une ligne de la liste 2D carre si toutes les lignes ont la même somme, et -1 sinon """
-    somme = sum(carre[0])
-    for ligne in range(len(carre)):
-        if (ligne != somme):
-            print("-1")
+    som = sum(carre[0])
+    for L in carre:
+        if sum(L) != som:
+            return -1
+    return som
 
 
 print(testLignesEgales(carre_mag))
+print()
 print(testLignesEgales(carre_pas_mag))
+print()
+
+
+def testColonnesEgales(carre):
+    """ Renvoie la somme des éléments d'une colonne de la liste 2D carre si toutes les colonnes ont la même somme, et -1 sinon """
+    col = [L[0] for L in carre]
+    som = sum(col)
+    for i in range(1, len(carre)):
+        col = [L[i] for L in carre]
+        if sum(col) != som:
+            return -1
+    return som
+
+
+print(testColonnesEgales(carre_mag))
+print()
+print(testColonnesEgales(carre_pas_mag))
+print()
+
+
+def testDiagonalesEgales(carre):
+    """ Renvoie la somme des éléments d'une diagonale de la liste 2D carre si les 2 diagonales ont la même somme, et -1 sinon """
+    diag = []
+    for i in range(len(carre)):
+        x = carre[i][i]
+        diag.append(x)
+        print(diag)
+    som = sum(diag)
+    diag2 = []
+    z = 3
+    for j in range(len(carre)):
+        y = carre[j][j + z]
+        z -= 1
+        diag2.append(y)
+        print(diag2)
+        if (sum(diag) != som):
+            return -1
+    return som
+
+
+'''def testDiagonalesEgales(carre):
+
+  n = len(carre)
+  diag = [carre[i][i] for i in range(n)]
+  anti_diag = [carre[i][n - 1 - i] for i in range(n)]
+  som = sum(diag)
+  if som == sum(anti_diag):
+    return som
+  else:
+    return -1
+    
+print(testDiagonalesEgales(carre_mag))
+print(testDiagonalesEgales(carre_pas_mag))'''
+
+
+print(testDiagonalesEgales(carre_mag))
+print()
+print(testDiagonalesEgales(carre_pas_mag))
+print()
+
+
+def estCarreMagique(carre):
+    """ Renvoie True si c'est un carre magique et False sinon"""
+    if (testLignesEgales(carre) == testColonnesEgales(carre) == testDiagonalesEgales(carre)):
+        return True
+    else:
+        return False
+
+
+print(estCarreMagique(carre_mag))
+print()
+print(estCarreMagique(carre_pas_mag))
+print()
--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -1,16 +1,59 @@
+# n = 3: (3; 10; 5; 16; 8; 4; 2; 1)
 def syracuse(n):
     """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
     liste = []
     while (n != 1):
-        if(n % 2 == 0):
-            x = n // 2
-            liste.append(x)
-        elif(n % 2 == 1):
-            x = n * 3 + 1
-            liste.append(x)
-        return liste
+        liste.append(n)
+        if (n % 2 == 0):
+            n = n // 2
+        elif (n % 2 == 1):
+            n = n * 3 + 1
+    liste.append(1)
     return liste
 
 
 print(syracuse(3))
-print(syracuse)
+
+
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
+    for i in range(2, n_max + 1):
+        syracuse(i)
+    return True
+
+
+print(testeConjecture(10000))
+
+
+def tempsVol(n):
+    """ Retourne le temps de vol de n """
+    syracuse(n)
+    temps_vol = len(syracuse(n)) - 1
+    return temps_vol
+
+
+print("Le temps de vol de", 3, "est", tempsVol(3))
+
+
+def tempsVolListe(n_max):
+    """ Retourne la liste de tous les temps de vol de 1 à n_max """
+    liste = []
+    for i in range(2, n_max):
+        liste.append(tempsVol(i))
+    return liste
+
+
+print(tempsVolListe(100))
+
+print(max(tempsVolListe(10000)))
+
+
+def altMax(n_max):
+    """ Retourner l'altitude maximale entre 2 et n_max"""
+    liste = []
+    for i in range(2, n_max + 1):
+        liste.extend(syracuse(i))
+    return max(liste)
+
+
+print(altMax(10000))
